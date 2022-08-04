968ca2d06d765c814d0f383bcec2ef4f308044bf
Random-Person280 <80506189+GrimReaper2654@users.noreply.github.com>
2022-08-04 11:32:23 +0800

    Update Thingo.js

--- Thingo.js
+++ Thingo.js
@@ -2,7 +2,16 @@ function init() {
     const player_name = 'Taj';
     const settings = {
         stat_limit: 1000,
-        
+        enable_shield: true,
+        enable_hunger: true,
+        rapid_regeneration: false,
+        weapon_durability: false,
+        secondary_ammo: true,
+        dmg_multilier: 1,
+        heal_multiplier: 1,
+        shield_protection_multiplier: 1,
+        shield_durability_multiplier: 1,
+        max_levels: 50,
     };
     const enemies = {
         name: {
