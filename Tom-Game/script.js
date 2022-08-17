5296d9be2057d21102724d9de1afb765888046d8
MQ-9 Reaper <85726678+Grim4Reaper@users.noreply.github.com>
2022-08-17 12:13:45 +0800

    Added Tom Game

---
+++ Tom-Game/script.js
@@ -0,0 +1,46 @@
+/*
+INFO
+
+*/
+
+
+// Variables
+
+
+
+
+
+
+// Functions
+
+function init() {
+
+}
+
+function showText(id, text) {
+    document.getElementById(id).innerHTML = text;
+}
+function addText(id, text) {
+    document.getElementById(id).innerHTML = document.getElementById("text").innerHTML + text;
+}
+function hideText(id) {
+    document.getElementById(id).style.display = "block"
+}
+function unhideText(id) {
+    document.getElementById(id).style.display = "block"
+}
+
+
+
+
+
+
+// Main code
+
+
+
+
+
+
+
+
