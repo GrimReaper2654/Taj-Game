3567e3494606c787f270ffd9bec7108fc12f5a11
GrimReaper2654 <80506189+GrimReaper2654@users.noreply.github.com>
2022-10-18 13:51:30 +0800

    assessment

---
+++ lmfao.js
@@ -0,0 +1,18 @@
+let school = {
+    buildingName: {
+        roomName: {
+            accessPoints: ['','',''],
+            routers: ['','',''],
+            switches: ['','',''],
+            computers: ['','','']
+        },
+    },
+    Gardam: {
+        comms: {
+            accessPoints: ['','',''],
+            routers: ['','',''],
+            switches: ['','',''],
+            computers: ['','','']
+        }
+    }
+};
